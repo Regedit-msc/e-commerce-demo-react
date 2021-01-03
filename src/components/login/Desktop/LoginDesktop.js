30ba46140f5d84ae6e4d32538c90be5fc8bae340
Samson <samsonudo@yahoo.com>
2021-01-03 23:23:11 +0100

    Updated firebase

--- src/components/login/Desktop/LoginDesktop.js
+++ src/components/login/Desktop/LoginDesktop.js
@@ -1,64 +1,24 @@
-import React from 'react'
-import Button from '@material-ui/core/Button';
-import Dialog from '@material-ui/core/Dialog';
-import DialogContent from '@material-ui/core/DialogContent';
-import useMediaQuery from '@material-ui/core/useMediaQuery';
-import { useTheme } from '@material-ui/core/styles';
-import bg from '../../assets/bg/Login-enterprise-bg.jpg';
-import Logo from '../../assets/bg/ibreward-icon-common.png';
-import './Modal.css';
-import firebase from '../../utils/firebase';
+import React from 'react';
+import firebase from '../../../utils/firebase';
 import TextField from '@material-ui/core/TextField';
-import Icon from '../../assets/bg/q-mark-forgotpass.png';
+import Icon from '../../../assets/bg/q-mark-forgotpass.png';
 import { FaFacebook, FaGoogle } from 'react-icons/fa';
+import Logo from '../../../assets/bg/ibreward-icon-common.png';
+import bg from '../../../assets/bg/Login-enterprise-bg.jpg';
+import './LoginDesktop.css';
 
-const Modal = () => {
+const LoginDesktop = () => {
     let email, firstname, lastname, password, gstn, phone;
-    const [open, setOpen] = React.useState(false);
-    const theme = useTheme();
-    const fullScreen = useMediaQuery(theme.breakpoints.down('sm'));
-
-    const handleClickOpen = () => setOpen(true);
 
     const [currIndex, setCurrIndex] = React.useState(0);
-
-    const handleClose = () => {
-        setOpen(false);
-        setCurrIndex(0);
-    };
-
-    let a = {
-        style: {
-            minHeight: "92vh",
-            minWidth: "60vw",
-            overflow: 'hidden',
-            background: `#fff url(${bg}) 0 100% no-repeat`,
-            display: 'flex',
-            position: 'fixed',
-            height: '100%',
-            width: '100%',
-        }
-    }
-
-    let b = {
-        style: {
-            minHeight: "30vh",
-            minWidth: "60vw",
-            overflow: 'hidden',
-            display: 'flex',
-            position: 'fixed',
-            height: '40%',
-            width: '100%',
-        }
-    }
-
+ 
     const LoginPage = () => {
         return (
-            <div style={{ display: 'flex' }}>
-                <div style={{ flex: '0.5', background: '#5469cb' }}>
+            <div style={{ display: 'flex', paddingRight: '10px'}}>
+                <div style={{ flex: '0.61', background: '#5469cb 100% 100%' }}>
                     <FirstPage />
                 </div>
-                <div style={{ flex: '0.15' }}></div>
+                <div style={{ flex: '0.04' }}></div>
                 <div style={{ flex: '0.35', width: '100%', background: '#fff' }}>
                     <SecondPage />
                 </div>
@@ -96,11 +56,11 @@ const Modal = () => {
                 <label>Email or Phone</label>
                 <TextField style={{ float: 'left', paddingLeft: '1px', width: '90%' }} placeholder="Enter email or phone" onChange={(e) => email = e.target.value} />
 
-                <p style={{ paddingTop: '10px', paddingBottom: '20px' }}></p>
+                <p style={{ paddingTop: '10px', paddingBottom: '20px', paddingTop: '30px' }}></p>
                 <label>Your Password</label>
                 <TextField style={{ float: 'left', paddingLeft: '1px', width: '90%' }} type="password" placeholder="**********" onChange={(e) => password = e.target.value} />
 
-                <p style={{ paddingBottom: '20px' }}></p>
+                <p style={{ paddingBottom: '20px', paddingTop: '30px' }}></p>
 
                 <button className="signin--button" onClick={login}>SIGN IN</button>
                 <p style={{ paddingTop: '10px', textAlign: 'center', fontSize: '13' }}>OR</p>
@@ -109,11 +69,11 @@ const Modal = () => {
                 <p></p>
                 <div className="social--text">Sign In with your social account!
                 <FaFacebook
-                        style={{ marginLeft: '10px',color: "blue", fontSize: "22px" }}
+                        style={{ marginLeft: '5px', color: "blue", fontSize: "22px" }}
                         onClick={facebookAuth}
                     />
-                     <FaGoogle
-                        style={{ marginLeft: '10px', color: "red", fontSize: "22px" }}
+                    <FaGoogle
+                        style={{ marginLeft: '7px', color: "red", fontSize: "22px" }}
                         onClick={googleAuth}
                     />
                 </div>
@@ -137,12 +97,12 @@ const Modal = () => {
 
     const ForgotPassword = () => {
         return (
-            <div style={{ display: 'flex' }}>
-                <div style={{ flex: '0.5', background: '#5469cb', height: '100%' }}>
+            <div style={{ display: 'flex' }} className="forgot--page">
+                <div style={{ flex: '0.63', background: '#5469cb', paddingTop: '10px' }}>
                     <FirstPageForgot />
                 </div>
-                <div style={{ flex: '0.10' }}></div>
-                <div style={{ flex: '0.40', width: '100%', background: '#fff' }}>
+                <div style={{ flex: '0.02' }}></div>
+                <div style={{ flex: '0.40', width: '100%', background: '#fff', paddingBottom: '10px'}}>
                     <SecondPageForgot />
                 </div>
             </div>
@@ -152,7 +112,7 @@ const Modal = () => {
 
     const FirstPageForgot = () => {
         return (
-            <div>
+            <div style={{ paddingLeft: '15px', paddingRight: '15px'}}>
                 <center>
                     <img src={Icon} alt="Icon" />
                     <div>Please enter your e-mail address and we will send you a confirmation mail to reset your password.</div>
@@ -163,11 +123,11 @@ const Modal = () => {
 
     const SecondPageForgot = () => {
         return (
-            <div>
+            <div className="second--page">
                 <p className="title">Forgot Password</p>
                 <label>Email Your Email</label>
                 <TextField style={{ float: 'left', paddingLeft: '1px', width: '90%' }} placeholder="Enter your email" onChange={(e) => email = e.target.value} />
-                <p style={{ paddingTop: '10px', paddingBottom: '20px' }}></p>
+                <p style={{ paddingTop: '30px', paddingBottom: '20px' }}></p>
                 <button className="signin--button" onClick={forgot}>SUBMIT</button>
             </div>
         );
@@ -177,10 +137,10 @@ const Modal = () => {
     const SignUp = () => {
         return (
             <div style={{ display: 'flex' }}>
-                <div style={{ flex: '0.5', background: '#5469cb' }}>
+                <div style={{ flex: '0.61', background: '#5469cb' }}>
                     <FirstPageSignUp />
                 </div>
-                <div style={{ flex: '0.15' }}></div>
+                <div style={{ flex: '0.04' }}></div>
                 <div style={{ flex: '0.35', width: '100%', background: '#fff' }}>
                     <SecondPageSignUp />
                 </div>
@@ -216,93 +176,84 @@ const Modal = () => {
             <div className="second--page">
                 <p className="title">Sign Up</p>
                 <label>First Name</label>
-                <TextField style={{ float: 'left', paddingLeft: '1px', width: '90%' }} placeholder="First Name" onChange={(e) => firstname = e.target.value}/>
+                <TextField style={{ float: 'left', paddingLeft: '1px', width: '90%' }} placeholder="First Name" onChange={(e) => firstname = e.target.value} />
 
-                <p style={{ paddingTop: '10px', paddingBottom: '20px' }}></p>
+                <p style={{ paddingTop: '30px', paddingBottom: '20px' }}></p>
 
                 <label>Last Name</label>
-                <TextField style={{ float: 'left', paddingLeft: '1px', width: '90%' }} placeholder="Last Name" onChange={(e) => lastname = e.target.value}/>
+                <TextField style={{ float: 'left', paddingLeft: '1px', width: '90%' }} placeholder="Last Name" onChange={(e) => lastname = e.target.value} />
 
-                <p style={{ paddingTop: '10px', paddingBottom: '20px' }}></p>
+                <p style={{ paddingTop: '30px', paddingBottom: '20px' }}></p>
 
                 <label>Email or Username</label>
-                <TextField style={{ float: 'left', paddingLeft: '1px', width: '90%' }} placeholder="Email or Username" onChange={(e) => email = e.target.value}/>
+                <TextField style={{ float: 'left', paddingLeft: '1px', width: '90%' }} placeholder="Email or Username" onChange={(e) => email = e.target.value} />
 
-                <p style={{ paddingTop: '10px', paddingBottom: '20px' }}></p>
+                <p style={{ paddingTop: '30px', paddingBottom: '20px' }}></p>
 
                 <label>Your Password</label>
-                <TextField style={{ float: 'left', paddingLeft: '1px', width: '90%' }} type="password" placeholder="**********" onChange={(e) => password = e.target.value}/>
+                <TextField style={{ float: 'left', paddingLeft: '1px', width: '90%' }} type="password" placeholder="**********" onChange={(e) => password = e.target.value} />
 
-                <p style={{ paddingBottom: '20px' }}></p>
+                <p style={{ paddingBottom: '20px',paddingTop: '30px' }}></p>
 
                 <label>Phone number</label>
-                <TextField style={{ float: 'left', paddingLeft: '1px', width: '90%' }} placeholder="Phone number" type="number" onChange={(e) => phone = e.target.value}/>
+                <TextField style={{ float: 'left', paddingLeft: '1px', width: '90%' }} placeholder="Phone number" type="number" onChange={(e) => phone = e.target.value} />
 
                 <p style={{ paddingTop: '10px', paddingBottom: '20px' }}></p>
 
                 <label>GSTN number</label>
-                <TextField style={{ float: 'left', paddingLeft: '1px', width: '90%' }} placeholder="GSTN number" type="number" onChange={(e) => gstn = e.target.value}/>
+                <TextField style={{ float: 'left', paddingLeft: '1px', width: '90%' }} placeholder="GSTN number" type="number" onChange={(e) => gstn = e.target.value} />
 
-                <p style={{ paddingTop: '20px', paddingBottom: '20px' }}></p>
+                <p style={{ paddingTop: '20px', paddingBottom: '30px' }}></p>
 
                 <button className="signin--button" onClick={register}>REGISTER NOW</button>
-                <p></p>
+                <p style={{paddingTop: '10px' }}></p>
                 <div className="center-text--signup" onClick={() => setCurrIndex(0)}>
                     Already registered?
                     <div className="signin--password--link">Sign in?</div></div>
+                    <p style={{paddingTop: '10px' }}></p>
             </div>
         );
     }
 
     const login = async () => {
         const cred = await firebase.auth().signInWithEmailAndPassword(email, password);
-        handleClose();
         alert('Welcome' + cred.user.email);
     }
 
     const forgot = async () => {
         const cred = await firebase.auth().sendPasswordResetEmail(email);
-        handleClose();
         alert('Reset email has been sent' + cred.user.email);
     }
 
     const googleAuth = async () => {
         const provider = new firebase.auth.GoogleAuthProvider();
         firebase.auth().signInWithPopup(provider).then(oAuth => {
-            handleClose();
-        alert('Welcome' + oAuth.user.email);
+            alert('Welcome' + oAuth.user.email);
         });
-        
+
     }
 
     const facebookAuth = async () => {
         const provider = new firebase.auth.FacebookAuthProvider();
         firebase.auth().signInWithPopup(provider).then(oAuth => {
-            handleClose();
-        alert('Welcome' + oAuth.user.email);
+            alert('Welcome' + oAuth.user.email);
         });
-        
+
     }
 
     const register = async () => {
         const cred = await firebase.auth().createUserWithEmailAndPassword(email, password);
         await firebase.firestore().collection('users').doc(cred.user.uid).set({ email, firstname, lastname, password, gstn, phone });
-        handleClose();
         alert('Welcome' + cred.user.email);
     }
 
     return (
         <div>
-            <Button variant="outlined" color="primary" onClick={handleClickOpen}>Open </Button>
-            <Dialog fullScreen={fullScreen} open={open} onClose={handleClose} PaperProps={currIndex === 2 ? b : a}>
-                <DialogContent >
-                    {currIndex == 0 && <LoginPage />}
-                    {currIndex == 1 && <SignUp />}
-                    {currIndex == 2 && <ForgotPassword />}
-                </DialogContent>
-            </Dialog>
+            {currIndex === 0 && <LoginPage />}
+            {currIndex === 1 && <SignUp />}
+            {currIndex === 2 && <ForgotPassword />}
         </div>
     );
-}
+};
 
-export default Modal;
+export default LoginDesktop;
---
+++ src/components/login/Login.js
@@ -0,0 +1,51 @@
+import React from 'react'
+import Dialog from '@material-ui/core/Dialog';
+import DialogContent from '@material-ui/core/DialogContent';
+import useMediaQuery from '@material-ui/core/useMediaQuery';
+import { useTheme } from '@material-ui/core/styles';
+import bg from '../../assets/bg/Login-enterprise-bg.jpg';
+import './Login.css';
+import LoginDesktop from './Desktop/LoginDesktop';
+import MobileLogin from './Mobile/MobileLogin';
+import { withRouter } from 'react-router-dom';
+
+const Login = ({ history }) => {
+    const [open, setOpen] = React.useState(true);
+    const theme = useTheme();
+    const fullScreen = useMediaQuery(theme.breakpoints.down('sm'));
+
+    const handleClose = () => {
+        setOpen(false);
+        history.goBack();
+    };
+
+    let a = {
+        style: {
+            minWidth: "63vw",
+            overflow: 'hidden',
+            background: `#fff url(${bg}) 0 100% no-repeat`,
+            display: 'flex',
+            position: 'fixed',
+            width: '100%',
+        }
+    }
+
+    return (
+        <div>
+            <Dialog fullScreen={fullScreen} open={open} onClose={handleClose} PaperProps={a}>
+                <DialogContent style={{padding: '0'}}>
+                    <div className="desktop--page" style={{ backgroundColor: 'white'}}>
+                    <div className="close" onClick={handleClose}>X</div>
+                        <LoginDesktop />
+                    </div>
+                    <div className="mobile--page">
+                    <div className="close--mobile" onClick={handleClose}>X</div>
+                        <MobileLogin />
+                    </div>
+                </DialogContent>
+            </Dialog>
+        </div>
+    );
+}
+
+export default withRouter(Login);
